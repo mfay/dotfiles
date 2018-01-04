483720a9f03c1ea3599e1973dc8b155c835ba947
Mark Fay <fay.mark@gmail.com>
2018-01-03 20:28:29 -0500

    added linker

---
+++ install/link.sh
@@ -0,0 +1,12 @@
+DOTFILES=$HOME/dotfiles
+
+linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+for file in $linkables; do
+	target="$HOME/.$( basename $file '.symlink' )"
+	if [ -e $target ]; then
+		echo "~${target#$HOME} already exists... Skipping."
+	else
+		echo "Creating symlink for $file"
+		ln -s $file $target
+	if
+done
