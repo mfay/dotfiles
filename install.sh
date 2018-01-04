5a813534cebf516aaacda94da03e4f27b970205e
Mark Fay <fay.mark@gmail.com>
2018-01-03 20:49:42 -0500

    added tmux install

--- install.sh
+++ install.sh
@@ -2,10 +2,15 @@ command_exists() {
 	type "$1" > /dev/null 2>&1
 }
 
-echo "installing..."
+echo "Installing..."
 
 source install/link.sh
 
 source install/git.sh
 
 source install/vim.sh
+
+if ! comamnd_exists tmux; then
+	echo "Installing tmux."
+	apt install -y tmux
+fi
